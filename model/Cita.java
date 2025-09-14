ea1d644b241dad0afe600bd65d9f2530bf33276d
Andres Rojas <and9193ro@gmail.com>
2025-09-14 17:18:29 -0500

    Update Cita.java
    Agregados nuevos metodos para la clase Cita

--- model/Cita.java
+++ model/Cita.java
@@ -48,8 +48,26 @@ public class Cita {
         this.paciente = paciente;
     }
     
-    //Metodos Adicionales
+//Metodos Adicionales
+    //confirmar y cancelar citas
+    public void confirmarCita() {
+        System.out.println("La cita ha sido confirmada para el paciente " + paciente.getName());
+    }
+
+    public void cancelarCita() {
+        System.out.println("La cita ha sido cancelada para el paciente " + paciente.getName());
+    }
+    //reprogramr la cita
+    public void reprogramarCita(Date nuevaFecha, Date nuevaHora) {
+        this.date = nuevaFecha;
+        this.time = nuevaHora;
+        System.out.println("La cita ha sido reprogramada para: " + nuevaFecha + " a las " + nuevaHora);
+    }
+    //resumen de la cita
+    public String resumen() {
+        return "Cita de " + paciente.getName() + " en " + location + 
+               " el " + date + " a las " + time;
+    }
+
+
 }
-
-
-
