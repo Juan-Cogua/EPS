438c83e7175a3f304164923284b45a87d871cdbf
Juan Sebastián Cogua Rosas <juan.cogua@uptc.edu.co>
2025-11-04 23:57:53 -0500

    Actualizar SangreIncompatibleException.java

--- scr/excepciones/SangreIncompatibleException.java
+++ scr/excepciones/SangreIncompatibleException.java
@@ -1,11 +1,19 @@
 package excepciones;
 
 /**
- * 
+ * Excepción lanzada para indicar que se ha intentado realizar una acción (como
+ * una transfusión o donación) donde los grupos sanguíneos son **incompatibles**
+ * de acuerdo con las reglas médicas o de negocio.
+ *
+ * @author Juan Cogua
+ * @version 2.0
  */
 public class SangreIncompatibleException extends Exception {
+
     /**
-     * @param message
+     * Construye una nueva SangreIncompatibleException con el mensaje de detalle especificado.
+     *
+     * @param message el mensaje de detalle que explica la incompatibilidad de la sangre.
      */
     public SangreIncompatibleException(String message) {
         super(message);
