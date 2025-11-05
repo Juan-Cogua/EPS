13abfbddbb389798f167dead39edc42c64b195b6
Juan Sebastián Cogua Rosas <juan.cogua@uptc.edu.co>
2025-11-04 23:57:27 -0500

    Actualizar TrasplanteInvalidoException.java

--- scr/excepciones/TrasplanteInvalidoException.java
+++ scr/excepciones/TrasplanteInvalidoException.java
@@ -1,11 +1,18 @@
 package excepciones;
 
 /**
- * 
+ * Excepción lanzada para indicar que la operación de **trasplante no es válida**
+ * debido a condiciones médicas, lógicas, o de incompatibilidad que no se cumplen.
+ *
+ * @author Juan Cogua
+ * @version 2.0
  */
 public class TrasplanteInvalidoException extends Exception {
+
     /**
-     * @param message
+     * Construye una nueva TrasplanteInvalidoException con el mensaje de detalle especificado.
+     *
+     * @param message el mensaje de detalle que describe la razón por la que el trasplante es inválido.
      */
     public TrasplanteInvalidoException(String message) {
         super(message);
