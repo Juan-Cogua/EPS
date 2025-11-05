047e8c987059ff233772b12e352f3d2ea35564d6
Juan Sebastián Cogua Rosas <juan.cogua@uptc.edu.co>
2025-11-04 22:53:02 -0500

    Actualizar DonanteMenorEdadException.java

--- scr/excepciones/DonanteMenorEdadException.java
+++ scr/excepciones/DonanteMenorEdadException.java
@@ -1,20 +1,37 @@
 package excepciones;
 
 /**
- * Excepción lanzada cuando un donante no cumple con la restricción mínima de edad (menor de 18 años).
+ * Excepción lanzada para indicar que un      donante no cumple con la restricción mínima de edad
+ * para realizar una donación (es menor de 18 años).
+ *
+ * @author Juan Cogua
+ * @version 2.0
  */
 public class DonanteMenorEdadException extends InvalidDataException {
+
     /**
-     * 
+     * Construye una nueva excepción DonanteMenorEdadException con un mensaje de detalle null.
      */
-    public DonanteMenorEdadException() { super(); }
+    public DonanteMenorEdadException() {
+        super();
+    }
+
     /**
-     * @param message
+     * Construye una nueva excepción DonanteMenorEdadException con el mensaje de detalle especificado.
+     *
+     * @param message el mensaje de detalle (guardado para su posterior recuperación).
      */
-    public DonanteMenorEdadException(String message) { super(message); }
+    public DonanteMenorEdadException(String message) {
+        super(message);
+    }
+
     /**
-     * @param message
-     * @param cause
+     * Construye una nueva excepción DonanteMenorEdadException con el mensaje de detalle y la causa especificados.
+     *
+     * @param message el mensaje de detalle (guardado para su posterior recuperación).
+     * @param cause la causa de la excepción (guardada para su posterior recuperación).
      */
-    public DonanteMenorEdadException(String message, Throwable cause) { super(message, cause); }
+    public DonanteMenorEdadException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
