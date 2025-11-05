1d03b6ad826a11bb2d27448c40457b5855ad78c8
Juan Sebastián Cogua Rosas <juan.cogua@uptc.edu.co>
2025-11-04 23:44:13 -0500

    Actualizar FechaInvalidaException.java

--- scr/excepciones/FechaInvalidaException.java
+++ scr/excepciones/FechaInvalidaException.java
@@ -1,12 +1,18 @@
 package excepciones;
 
 /**
- * Excepción lanzada cuando se ingresa una fecha invalida.
+ * Excepción lanzada para indicar que se ha ingresado una **fecha no válida**
+ * o que no cumple con el formato o las restricciones lógicas esperadas.
+ *
+ * @author Juan Cogua
+ * @version 2.0
  */
 public class FechaInvalidaException extends Exception {
+    
     /**
-     * constructor de la clase que crea el mensaje de excepción.
-     * @param message
+     * Construye una nueva excepción FechaInvalidaException con el mensaje de detalle especificado.
+     *
+     * @param message El mensaje de detalle que describe la causa de la fecha inválida (guardado para su posterior recuperación).
      */
     public FechaInvalidaException(String message) {
         super(message);
