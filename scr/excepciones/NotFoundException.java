ba8281fc7da86155482ec1eb0168629b7d8106ed
Juan Sebastián Cogua Rosas <juan.cogua@uptc.edu.co>
2025-11-04 23:58:24 -0500

    Actualizar NotFoundException.java

--- scr/excepciones/NotFoundException.java
+++ scr/excepciones/NotFoundException.java
@@ -1,11 +1,9 @@
+
 package excepciones;
 
 /**
- * Excepción lanzada para indicar que un **recurso requerido no ha sido encontrado**
- * en el sistema (por ejemplo, un Paciente, un Donante, un registro en la base de datos, etc.).
- *
- * Esta es una excepción no comprobada ({@code RuntimeException}) que se usa
- * para señalar que una búsqueda o recuperación de datos falló.
+ * Excepción lanzada para indicar que un recurso requerido no ha sido encontrado
+ * en el sistema (por ejemplo, Paciente o Donante).
  *
  * @author Juan Cogua
  * @version 2.0
@@ -32,8 +30,7 @@ public class NotFoundException extends RuntimeException {
      * Construye una nueva NotFoundException con el mensaje de detalle y la causa especificados.
      *
      * @param message el mensaje de detalle.
-     * @param cause la causa de la excepción (guardada para su posterior recuperación),
-     * útil si la causa subyacente fue otra excepción (como una excepción de SQL).
+     * @param cause la causa de la excepción (guardada para su posterior recuperación).
      */
     public NotFoundException(String message, Throwable cause) {
         super(message, cause);
